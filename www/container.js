3269f7bdb1c3d87b7d75e5a102da0d74fd03f286
Roderic Page <rdmpage@gmail.com>
2019-09-16 12:57:53 +0100

    Container template

---
+++ www/container.js
@@ -0,0 +1,160 @@
+var template_container = `
+
+<%
+
+// Define functions in template, see https://stackoverflow.com/a/40968695/9684
+
+//----------------------------------------------------------------------------------------
+// Get a single string from a literal that may have multilingual values
+get_literal = function(key) {
+	var literal = '';
+	
+	console.log("get_literal key = " + JSON.stringify(key));
+	
+	// literal is a simple string
+	if (typeof key === 'string') {
+		literal = key;
+	}
+	
+	// name is an object so we have one language stored in @value
+	if (typeof key === 'object' && !Array.isArray(key)) {	
+		literal = key['@value'];
+	} else {
+		// name is an array of objects and/or strings (need to think about this)
+		if (Array.isArray(key)) {
+		
+		    // just take first
+			// literal = key[0]['@value'];
+			
+			var strings = [];
+			for (var i in key) {
+				strings.push(key[i]['@value']);
+			}
+			literal = strings.join(' / ');			
+		}
+	}
+	
+	return literal;
+}
+
+//----------------------------------------------------------------------------------------
+// Convert ISO data to a human-readable string (PubMed-style)
+// My databases use -00 to indicate no month or no day, and this confuses Javascript
+// Date so we need to set the options appropriately
+isodate_to_string = function (datestring) {
+
+	// By default assume datestring is a year only
+	var options = {};
+	options.year = 'numeric';
+	
+	// Test for valid month, then day (because we use -00 to indicate no data)
+	var m = null;
+	
+	if (!m) {	
+		m = datestring.match(/^([0-9]{4})$/);
+		if (m) {
+			// year only
+			datestring = m[1]; 
+		}
+	}
+	
+	if (!m) {		
+		m = datestring.match(/^([0-9]{4})-([0-9]{2})-00/);
+		if (m) {
+			
+			if (m[2] == '00') {
+				// Javascript can't handle -00-00 date string so set to January 1st 
+				// which won't be output as we're only outputting the year
+				datestring = m[1] + '-01-01';
+			} else {
+				// We have a month but no day
+				datestring = m[1] + '-' + m[2] + '-01';
+				options.month = 'short';
+			}		
+		}
+	}
+	
+	if (!m) {	
+		m = datestring.match(/^([0-9]{4})-([0-9]{2})-([0-9]{2})/);
+		if (m) {
+			// we have yea, month, and day
+			options.month = 'short';
+			options.day = 'numeric';
+		}
+	}
+	
+	var d = new Date(datestring);
+	datestring = d.toLocaleString('en-gb', options);
+	
+	return datestring;		
+}
+
+//----------------------------------------------------------------------------------------
+
+get_property_value = function(key, propertyID) {
+	var value = '';
+	
+	if (typeof key === 'object' && !Array.isArray(key)) {
+		if (key.propertyID === propertyID) {	
+			value = key.value;
+		}
+	} else {
+		if (Array.isArray(key)) {
+			for (var i in key) {
+				if (key[i].propertyID === propertyID) {
+					value = key[i].value;
+				}
+			}
+		}	
+	}
+	
+	return value;
+}
+
+
+if (data['@graph']) {
+	data = data['@graph'][0];
+}
+
+var item = null;
+
+item = data;
+
+
+%>
+
+
+<!-- title -->
+<h1>
+	<%= get_literal(item.name) %>
+</h1>
+
+<!-- identifiers -->
+<div>
+<% if (item.issn) {
+
+	var issn_string = '';
+	
+	if (typeof item.issn === 'string') {
+		issn_string = item.issn ;
+	}
+	
+	if (Array.isArray(item.issn )) {
+		var issn = [];
+		for (var i in item.issn) {
+			issn.push(item.issn[i]);
+		}
+		issn_string = issn.join('; ');
+	}
+	%>
+	
+	<span class="heading">ISSN:</span>
+	<%- issn_string %>
+	<%  
+} %>
+</div>
+
+
+
+
+`;
