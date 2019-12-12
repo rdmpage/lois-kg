854e1a96e09ccca421837c9035202f75f3a1b800
Roderic Page <rdmpage@gmail.com>
2019-12-12 15:10:22 +0000

    oz-orcid

--- oz-orcid/backup.sh
+++ oz-orcid/backup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl http://167.71.255.145:5984/oz-orcid/_design/export/_list/values/jsonl > backup.jsonl
+curl http://localhost:32775/oz-orcid/_design/export/_list/values/jsonl > backup.jsonl
